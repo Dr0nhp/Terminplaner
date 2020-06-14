7881d92f3f2969197ccce2f6d6069d2c85ac4dfb
Dr0nhp <mxoc@hotmail.de>
2020-06-14 17:00:09 +0200

    Datumsberechnung funktioniert Aufstieg in 0.2 TODO: Nurnoch sinnvolle Terminanlagen gültig machen

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ Array.prototype.hasMin = function(attrib) {
 /************************************* start of discord bot Terminplaner *************************************/
 
 
-v = "Version 0.1"
+v = "Version 0.2"
 const helptext = "Hallo!\nMit !befehle erhälst du eine Liste mit Befehlen.\nEs ist Egal, ob du deine Befehle GROSS oder klein schreibst.\nUm einen Termin anzulegen tippe:\n \"!Termin Terminname TeilnehmendePersonen Datum Uhrzeit\"\n ein.";
 const befehle = "Folgende Befehle stehen derzeit zur Verfügung:\n!ping\n!hilfe\n!befehle\n!termin\n!version\n!speichern\n!nächster\n!alle";
 
@@ -96,13 +96,14 @@ function termin(argsArray) {
 		status = "Die Terminerstellung war nicht erfolgreich.\nBitte nutze 4 Argumente zum erstellen von einem Termin.\n!Hilfe gibt dir eine Hilfestellung."
 		return status;
 	}
-
+	//parseTime(argsArray[2],argsArray[3])
+	console.log(typeof(argsArray[3]))
 	Event = {
 		Terminname: argsArray[0],
 		Teilnehmer: argsArray[1],
 		Datum: argsArray[2],
 		Uhrzeit: argsArray[3],
-		hidden: argsArray[4]
+		hidden: parseTime(argsArray[2],argsArray[3])
 	};
 	jsonData = JSON.stringify(Event);
 	eventArray.push(jsonData)
@@ -122,9 +123,37 @@ function save(eventArray) {
 
 
 function next(eventArray) {
-	n = eventArray.hasMin("Datum")
+	n = eventArray.hasMin("hidden")
 	return(n)
 }
+
+
+function parseTime(date,time) {
+
+	//expected dateformat dd.mm.(yyyy)
+	dateArr = date.split('.')
+	day = dateArr[0];
+	month = parseInt(dateArr[1])-1
+	year = dateArr[2];
+
+	// if no year is given
+	if (year == undefined){
+		actualDate = new Date();
+  	actualyear = actualDate.getFullYear();
+	  year = actualyear.toString();
+	}
+
+	//time hh:mm
+	timeArr = time.split(':')
+	hh = timeArr[0];
+	min = timeArr[1];
+
+	givenDate =new Date(year,month,day,hh,min)
+
+	timeNumber = Date.parse(givenDate);
+	return timeNumber
+}
+
 
 
 client.login(token);
